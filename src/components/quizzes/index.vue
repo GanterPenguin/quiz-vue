e0bc3c5b21b5453d619cb6a07cb8f8038739b24e
BuldakovIvan <iv081198@yandex.ru>
2019-07-24 12:24:43 +0300

    add components

---
+++ src/components/quizzes/index.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapGetters, mapActions, mapState } from 'vuex';
+
+export default {
+
+    components: {
+    },
+    data: function () {
+        return {
+        }
+    },
+    methods: {
+        ...mapActions('quizzes', [
+        ]),
+    },
+    computed: {
+        ...mapState({
+        }),
+    },
+}
+</script>
+
+
+<template lang="pug">
+
+.quizzes
+
+</template>
--- src/index.js
+++ src/index.js
@@ -9,13 +9,23 @@ import { mapActions } from 'vuex';
 import Layout from './layout';
 import store from './store';
 
+import Quizzes from './components/quizzes';
+import Quiz from './components/quiz';
+
+
 export default class QuizApp {
 
     constructor(selector) {
 
         const el = document.querySelector(selector);
 
-        //const router = new VueRouter({ routes });
+        const routes = [
+            { path: '/', component: Quizzes },
+            { path: '/quiz/:id', component: Quiz },
+        ];
+
+        const router = new VueRouter({ routes });
+
         Vue.use(VueRouter);
 
         let vm = new Vue({
